008f871999bd79789fafea1dd452f3d0a802ec2a
Guillaume Lerda <guillaume.lerda@itsf.io>
2025-04-28 23:49:39 +0200

    Handle list of events
    (cherry picked from commit ab3b6355ac55ade90705cc07cb7aa6bafb09dc32)

--- src/main/java/org/example/domain/BulbEvent.java
+++ src/main/java/org/example/domain/BulbEvent.java
@@ -1,7 +1,6 @@
 package org.example.domain;
 
 public enum BulbEvent {
-    NO_EVENT,
     SWITCHED_ON,
     SWITCHED_OFF,
     BROKE
--- src/main/java/org/example/domain/LedBulb.java
+++ src/main/java/org/example/domain/LedBulb.java
@@ -3,6 +3,9 @@ package org.example.domain;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.Collections;
+import java.util.List;
+
 @AllArgsConstructor
 @Getter
 public class LedBulb implements Bulb {
@@ -31,26 +34,25 @@ public class LedBulb implements Bulb {
     }
 
     public BulbState switchState(BulbState state, BulbCommand command) {
-        var event = decide(state, command);
-        return evolve(state, event);
+        var events = decide(state, command);
+        return evolveAll(state, events);
     }
 
-    private static BulbEvent decide(BulbState state, BulbCommand command) {
-        var event = BulbEvent.NO_EVENT;
+    private static List<BulbEvent> decide(BulbState state, BulbCommand command) {
         if (BulbCommand.SWITCH_ON.equals(command)) {
             if (!state.broken() && !state.on()) {
                 if (state.remainingUses() == 0) {
-                    event = BulbEvent.BROKE;
+                    return List.of(BulbEvent.BROKE);
                 } else {
-                    event = BulbEvent.SWITCHED_ON;
+                    return List.of(BulbEvent.SWITCHED_ON);
                 }
             }
         } else {
             if (!state.broken() && state.on()) {
-                event = BulbEvent.SWITCHED_OFF;
+                return List.of(BulbEvent.SWITCHED_OFF);
             }
         }
-        return event;
+        return Collections.emptyList();
     }
 
     private static BulbState evolve(BulbState state, BulbEvent event) {
@@ -58,8 +60,15 @@ public class LedBulb implements Bulb {
             case BROKE -> new BulbState(false, true, 0);
             case SWITCHED_ON -> new BulbState(true, false, state.remainingUses() - 1);
             case SWITCHED_OFF -> new BulbState(false, false, state.remainingUses());
-            default -> new BulbState(state.on(), state.broken(), state.remainingUses());
         };
+    }
+
+    private static BulbState evolveAll(BulbState initialState, List<BulbEvent> events) {
+        var state = new BulbState(initialState.on(), initialState.broken(), initialState.remainingUses());
+        for (BulbEvent event : events) {
+            state = evolve(state, event);
+        }
+        return state;
     }
 
     @Override
