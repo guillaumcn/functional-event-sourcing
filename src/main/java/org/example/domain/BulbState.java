96abf36e243b2110cd42c85146ec649326664a0a
Guillaume Lerda <guillaume.lerda@itsf.io>
2025-04-28 23:31:22 +0200

    Move switchState to a pure function

---
+++ src/main/java/org/example/domain/BulbState.java
@@ -0,0 +1,4 @@
+package org.example.domain;
+
+public record BulbState(boolean on, boolean broken, int remainingUses) {
+}
--- src/main/java/org/example/domain/LedBulb.java
+++ src/main/java/org/example/domain/LedBulb.java
@@ -8,33 +8,52 @@ import lombok.Getter;
 public class LedBulb implements Bulb {
 
     private String name;
-    private boolean on;
-    private boolean broken;
-    private int remainingUses;
+    private BulbState state;
 
     public LedBulb(String name) {
         this.name = name;
-        this.on = false;
-        this.broken = false;
-        this.remainingUses = 3;
+        this.state = new BulbState(false, false, 2);
+    }
+
+    public LedBulb(String name, boolean on, boolean broken, int remainingUses) {
+        this.name = name;
+        this.state = new BulbState(on, broken, remainingUses);
     }
 
     @Override
     public void switchOn() {
-        if (!this.broken && !this.on) {
-            if (this.remainingUses == 0) {
-                this.broken = true;
-            } else {
-                this.on = true;
-                this.remainingUses--;
-            }
-        }
+        state = switchState(state, true);
     }
 
     @Override
     public void switchOff() {
-        if (!this.broken && this.on) {
-            this.on = false;
+        state = switchState(state, false);
+    }
+
+    public BulbState switchState(BulbState state, boolean on) {
+        if (on) {
+            if (!state.broken() && !state.on()) {
+                if (state.remainingUses() == 0) {
+                    return new BulbState(false, true, 0);
+                } else {
+                    return new BulbState(true, false, state.remainingUses() - 1);
+                }
+            }
+        } else {
+            if (!state.broken() && state.on()) {
+                return new BulbState(false, false, state.remainingUses());
+            }
         }
+        return new BulbState(state.on(), state.broken(), state.remainingUses());
+    }
+
+    @Override
+    public boolean isOn() {
+        return state.on();
+    }
+
+    @Override
+    public boolean isBroken() {
+        return state.broken();
     }
 }
